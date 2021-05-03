c23e6a41e1f1228c89c6493408a27cda1ae1ad26
Jana Voborská <jana.dostalov@seznam.cz>
2021-05-03 09:32:30 +0200

    pokus o fce na identifikaci policek

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ console.log(kdoNaTahu);
 //console.log(buttonElm); je to pole - musim proiterovat for cyklem
 
 const polozky = document.querySelectorAll('.policko');
+console.log(polozky);
+console.log(polozky[3]);
 
 for (let i = 0; i < polozky.length; i++) {
   // Tady můžeš dělat s každou položkou co chceš. Třeba jim všem přidat event listener
@@ -34,9 +36,29 @@ for (let i = 0; i < polozky.length; i++) {
   });
 }
 
+for (let i = 0; i < polozky.length; i++) {
+  const getSymbol = (polozky) => {
+    if (polozky[i].classList.contains('board__field--cross')) {
+      return 'cross';
+    } else if (polozky[i].classList.contains('board__field--circle')) {
+      return 'circle';
+    } else {
+      return 'undefined';
+    }
+  };
+}
+
+console.log(getSymbol(polozky[1]));
+
+const boardSize = 10; // 10x10
+//to jsou moje polozky, const fields = document.querySelectorAll('.policko');
+
+for (let i = 0; i < polozky.length; i++) {
+  const getField = (row, column) => {
+    polozky[i][row * boardSize + column];
+  };
+}
+
 /*
-
-Pomocí animací přidej symbolům efekt postupného zvětšení. V náhledu je animace zpomalená z času 0.15s na 0.45s, aby bylo lépe vidět, jak má vypadat. Ve výsledku použij kratší čas.
-
 
 */
